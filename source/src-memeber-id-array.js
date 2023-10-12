c4ed63615515613c30e72dc0a2c0728a101145ca
kongukjae <kongukjae@gmail.com>
2023-10-12 13:54:10 +0900

    기초 데이터 제작

---
+++ source/src-memeber-id-array.js
@@ -0,0 +1,20 @@
+module.exports = [
+  "kongukjae",
+  "kimwoojin123",
+  "forhn37",
+  "seunghee1108",
+  "dev-honing",
+  "samuel21695",
+  "S0ngYoungJun",
+  "personaprg",
+  "seungmin21",
+  "Yusmin97",
+  "YoonJieut",
+  "pingulee",
+  "Yuan12131",
+  "LeeEunJeong0731",
+  "JungYoungSick",
+  "csm009177",
+  "ChoiEunCheol",
+  "hmoongi94"
+];

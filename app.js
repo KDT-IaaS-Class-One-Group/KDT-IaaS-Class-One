e39e248dd4e22e4fbc337002062c81b6cb2d9587
승희 <141980284+seunghee1108@users.noreply.github.com>
2023-10-12 12:52:23 +0900

    유안아

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ http.createServer(function(request, response) {
     'Content-Type': 'text/html'
   }
   response.writeHead(200, writeHaedObject);
-  response.end("hello");
+  response.end("안녕유안아");
   
 }).listen(8080); // 포트설정
+

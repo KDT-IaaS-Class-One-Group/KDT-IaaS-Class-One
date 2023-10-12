0de107c7c8b7cbb31813dccfc3c336b2a1b998d1
leemingoo <dwmapi@naver.com>
2023-10-12 12:08:30 +0900

    프로젝트 시작

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
+const http = require("http");
+http.createServer(function(request, response) {
+  let writeHaedObject = {
+    'Content-Type': 'text/html'
+  }
+  response.writeHead(200, writeHaedObject);
+  response.end("hello");
   
-});
-
-server.listen(8080);
+}).listen(8080); // 포트설정
